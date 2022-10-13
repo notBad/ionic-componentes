8120f09e11eccff73f9f16f259368cf4dbbab364
JoseL <firing4@gmail.com>
2022-10-13 10:44:36 +0200

    action sheet

--- src/app/pages/action-sheet/action-sheet.page.ts
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -9,43 +9,57 @@ import { ActionSheetController } from '@ionic/angular';
 export class ActionSheetPage implements OnInit {
 
   result: string;
-
+  //inyectado servicio
   constructor(private actionSheetCtrl: ActionSheetController) {}
 
   ngOnInit() {
   }
 
+  onClick() {
+    this.presentActionSheet();
+  }
+  //método que crea ActionSheet
+  //es necesario que sea asincrono porque hay interacción con usuario
   async presentActionSheet() {
     const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Example header',
-      subHeader: 'Example subheader',
-      buttons: [
-        {
-          text: 'Delete',
-          role: 'destructive',
-          data: {
-            action: 'delete',
-          },
-        },
-        {
-          text: 'Share',
-          data: {
-            action: 'share',
-          },
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          data: {
-            action: 'cancel',
-          },
-        },
-      ],
+      header: 'Albumes',
+      backdropDismiss: false,//rechazar hacer click fuera
+      buttons: [{
+        text: 'Borrar',
+        role: 'destructive', //para ios (saldrá en rojo)
+        icon: 'trash-outline', //nombre de icono de ionic
+        cssClass: 'rojo', //clase normal de ccs
+        handler: () => { //lo que hará al ser pulsado
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share-outline',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle-outline',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart-outline',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close-outline',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
     });
-
     await actionSheet.present();
-
-    const result = await actionSheet.onDidDismiss();
-    this.result = JSON.stringify(result, null, 2);
   }
-}
+
+ }
