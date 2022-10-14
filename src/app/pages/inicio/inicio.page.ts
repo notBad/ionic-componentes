42b704efbb4fc2aa47b3cbee7c25ddacc2acd2e1
JoseL <firing4@gmail.com>
2022-10-14 14:45:09 +0200

    menus

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) { }
 
   ngOnInit() {
+  }
+
+  mostrarMenu(){
+    this.menuCtrl.open('first');
   }
 
 }
